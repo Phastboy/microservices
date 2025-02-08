d0c1c8023ec49dd95b618eafe0e5aa5eba2fa39d
Hammed Anuoluwapo Pelumi <stationphast@gmail.com>
2025-02-08 03:00:04 +0100

    added resolver

--- gateway/src/app.module.ts
+++ gateway/src/app.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { GraphqlModule } from './graphql/graphql.module';
 import { RmqClientModule } from './rmq-client/rmq-client.module';
+import { UserResolver } from './user.resolver';
 
 @Module({
     imports: [GraphqlModule, RmqClientModule],
-    providers: [],
+    providers: [UserResolver],
 })
 export class AppModule {}
