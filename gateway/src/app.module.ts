6a138f441d7e7fc87e6a5478a7f1d6bcb98f859c
Hammed Anuoluwapo Pelumi <stationphast@gmail.com>
2025-02-08 02:26:44 +0100

    rmq module added

--- gateway/src/app.module.ts
+++ gateway/src/app.module.ts
@@ -1,8 +1,9 @@
 import { Module } from '@nestjs/common';
 import { GraphqlModule } from './graphql/graphql.module';
+import { RmqClientModule } from './rmq-client/rmq-client.module';
 
 @Module({
-  imports: [GraphqlModule],
-  providers: [],
+    imports: [GraphqlModule, RmqClientModule],
+    providers: [],
 })
 export class AppModule {}
