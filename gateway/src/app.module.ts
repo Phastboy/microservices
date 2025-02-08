9d2074f3acf9df3bbe16dd94d032ca1574fb1f6e
Hammed Anuoluwapo Pelumi <stationphast@gmail.com>
2025-02-08 03:39:07 +0100

    enviroment config

--- gateway/src/app.module.ts
+++ gateway/src/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { GraphqlModule } from './graphql/graphql.module';
 import { RmqClientModule } from './rmq-client/rmq-client.module';
 import { UserResolver } from './user.resolver';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-    imports: [GraphqlModule, RmqClientModule],
+    imports: [GraphqlModule, RmqClientModule, ConfigModule.forRoot()],
     providers: [UserResolver],
 })
 export class AppModule {}
