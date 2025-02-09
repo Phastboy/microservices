a6c9bd6d80ed2443e273cb042584fa84de8fb5de
Hammed Anuoluwapo Pelumi <stationphast@gmail.com>
2025-02-09 05:02:20 +0100

    seperate users thing

--- gateway/src/app.module.ts
+++ gateway/src/app.module.ts
@@ -1,11 +1,11 @@
 import { Module } from '@nestjs/common';
 import { GraphqlModule } from './graphql/graphql.module';
 import { RmqClientModule } from './rmq-client/rmq-client.module';
-import { UserResolver } from './user.resolver';
 import { ConfigModule } from '@nestjs/config';
+import { UsersModule } from './users/users.module';
 
 @Module({
-    imports: [GraphqlModule, RmqClientModule, ConfigModule.forRoot()],
-    providers: [UserResolver],
+    imports: [GraphqlModule, RmqClientModule, ConfigModule.forRoot(), UsersModule],
+    providers: [],
 })
 export class AppModule {}
--- gateway/types/graphql.ts
+++ gateway/types/graphql.ts
@@ -8,13 +8,29 @@
 /* tslint:disable */
 /* eslint-disable */
 
+export interface CreateUserInput {
+    id: string;
+    name: string;
+}
+
+export interface UpdateUserInput {
+    id: string;
+}
+
 export interface User {
     id: string;
     name: string;
 }
 
 export interface IQuery {
+    users(): Nullable<User>[] | Promise<Nullable<User>[]>;
     user(id: string): Nullable<User> | Promise<Nullable<User>>;
+}
+
+export interface IMutation {
+    createUser(createUserInput: CreateUserInput): User | Promise<User>;
+    updateUser(updateUserInput: UpdateUserInput): User | Promise<User>;
+    removeUser(id: string): Nullable<User> | Promise<Nullable<User>>;
 }
 
 type Nullable<T> = T | null;
