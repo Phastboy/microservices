142c9ba0eb556ccd8b3dfd959fb28efb31222376
Hammed Anuoluwapo Pelumi <stationphast@gmail.com>
2025-02-09 10:01:38 +0100

    log received request

--- user-service/src/app.controller.ts
+++ user-service/src/app.controller.ts
@@ -1,13 +1,15 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Logger } from '@nestjs/common';
 import { MessagePattern } from '@nestjs/microservices';
 import { AppService } from './app.service';
 
 @Controller()
 export class AppController {
     constructor(private service: AppService) {}
+    private logger = new Logger(AppController.name);
 
     @MessagePattern('find_all_users')
     findAll() {
+        this.logger.log('request to fetch all users received from the client');
         return this.service.findAll();
     }
 
