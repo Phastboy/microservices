c93ef4987f3d630543872cf5badcc242a3484fe4
Anuoluwapo Pelumi Hammed <stationphast@gmail.com>
2025-02-20 02:41:40 +0100

    microservice

--- apps/users/src/main.ts
+++ apps/users/src/main.ts
@@ -1,8 +1,19 @@
 import { NestFactory } from '@nestjs/core';
 import { UsersModule } from './users.module';
+import { MicroserviceOptions, Transport } from '@nestjs/microservices';
 
 async function bootstrap() {
-  const app = await NestFactory.create(UsersModule);
-  await app.listen(process.env.port ?? 3000);
+  const app = await NestFactory.createMicroservice<MicroserviceOptions>(
+    UsersModule,
+    {
+      transport: Transport.RMQ,
+      options: {
+        urls: [process.env.RABBITMQ_URL || 'amqp://localhost:5672'],
+        queue: 'user_queue',
+        queueOptions: { durable: false },
+      },
+    },
+  );
+  await app.listen();
 }
 bootstrap();
