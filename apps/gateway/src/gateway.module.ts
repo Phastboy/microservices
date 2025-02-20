37242f92dd05fb52900910dbb33d8143a3a987e0
Anuoluwapo Pelumi Hammed <stationphast@gmail.com>
2025-02-20 02:22:13 +0100

    setup graphql

--- apps/gateway/src/gateway.module.ts
+++ apps/gateway/src/gateway.module.ts
@@ -1,7 +1,10 @@
 import { Module } from '@nestjs/common';
+import { RmqClientModule } from './rmq-client/rmq-client.module';
+import { GraphqlModule } from './graphql/graphql.module';
+import { UsersModule } from './users/users.module';
 
 @Module({
-  imports: [],
+  imports: [RmqClientModule, GraphqlModule, UsersModule],
   controllers: [],
   providers: [],
 })
--- apps/gateway/src/graphql/graphql.module.ts
+++ apps/gateway/src/graphql/graphql.module.ts
@@ -1,4 +1,17 @@
 import { Module } from '@nestjs/common';
+import { GraphQLModule } from '@nestjs/graphql';
+import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
+import { join } from 'path';
 
-@Module({})
+@Module({
+  imports: [
+    GraphQLModule.forRoot<ApolloDriverConfig>({
+      driver: ApolloDriver,
+      typePaths: ['./**/*.graphql'],
+      definitions: {
+        path: join(process.cwd(), '@/apps/gateway/src/graphql/graphql.ts'),
+      },
+    }),
+  ],
+})
 export class GraphqlModule {}
