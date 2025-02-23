8070c0d2c8725449f94217b771e7ca365c114292
Anuoluwapo Pelumi Hammed <stationphast@gmail.com>
2025-02-23 23:47:49 +0100

    rmq module setup for users queue

--- apps/users/src/main.ts
+++ apps/users/src/main.ts
@@ -9,7 +9,7 @@ async function bootstrap() {
       transport: Transport.RMQ,
       options: {
         urls: [process.env.RABBITMQ_URL || 'amqp://localhost:5672'],
-        queue: 'user_queue',
+        queue: 'users_queue',
         queueOptions: { durable: false },
       },
     },
